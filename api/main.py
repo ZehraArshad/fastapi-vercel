e17c40b98a7600594cd0f68fa30d609ac5496851
Zehra <zehraarshadmulla@gmail.com>
2025-08-30 17:54:51 +0500

    first commit

---
+++ api/main.py
@@ -0,0 +1,12 @@
+# main.py
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello from FastAPI on Vercel 🚀"}
+
+@app.get("/hello/{name}")
+def read_item(name: str):
+    return {"message": f"Hello, {name}!"}
